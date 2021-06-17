c0a08ed6d79b5b2967aa7105e9c9dc7fb934af8e
ivanneto02 <72827265+ivanneto02@users.noreply.github.com>
2021-06-17 15:39:22 -0700

    added to unctional_api.py file

--- keras/functional_api.py
+++ keras/functional_api.py
@@ -1,11 +1,33 @@
 import numpy as numpy
 import tensorflow as tf
 from tensorflow import keras
-from keras import layers
+from tensorflow.keras import layers
 
 def main():
     inputs = keras.Input(shape=(784,))
-    print(inputs)
+
+    #img_inputs = keras.Input(shape = (32, 32, 3))
+
+    #print(inputs.shape, inputs.dtype)
+    #print(img_inputs.shape, img_inputs.dtype)
+
+    dense = layers.Dense(64, activation='relu')
+
+    # Layer 1
+    x = dense(inputs)
+
+    # Layer 2
+    x = layers.Dense(64, activation='relu')(x)
+
+    # Output layer
+    outputs = layers.Dense(10)(x)
+
+    model = keras.Model(inputs=inputs, outputs=outputs, name="mnist_model")
+
+    model.summary()
+
+    keras.utils.plot_model(model, "./model_images/my_first_model.png")
+    keras.utils.plot_model(model, "./model_images/my_first_model_with_shape_info.png", show_shapes=True)
 
 if __name__ == "__main__":
     main()
